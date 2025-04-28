31cc62c96da962c8008f358028bf529ca16f34d9
Jamal Sorani <gamalsalmsorani18@gmail.com>
2025-04-28 09:56:07 +0300

    chore(android): update gradle version to 8.10.2 and address related issues
    This commit updates the Gradle version used in the Android project to 8.10.2.  This resolves several build issues and improves compatibility.  Additionally, it updates the `gradlew` script for improved functionality and security.  Finally, it updates the AndroidManifest.xml to request the necessary permission.

--- lib/method_channel_flutter_screensharing_restrict.dart
+++ lib/method_channel_flutter_screensharing_restrict.dart
@@ -3,17 +3,17 @@ import 'package:flutter/services.dart';
 
 import 'flutter_screensharing_restrict_platform.dart';
 
-
-
 /// An implementation of [FlutterScreensharingRestrictPlatform] that uses method channels.
-class MethodChannelFlutterScreensharingRestrict extends FlutterScreensharingRestrictPlatform {
+class MethodChannelFlutterScreensharingRestrict
+    extends FlutterScreensharingRestrictPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('detect_screen_sharing');
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
